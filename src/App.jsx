272ccf8842f41b3386c1fb9c4f8fa7ecef80640e
김준희 <shadd3338@gmail.com>
2025-07-24 15:09:17 +0900

    채팅페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './page/Home'
 import { Login } from './page/Login';
 import { Layout } from './components/layouts/Layout'
+import { ChatPage } from "./page/Chat";
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/chat" element={<ChatPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
